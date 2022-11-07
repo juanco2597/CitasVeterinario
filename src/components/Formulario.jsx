dd9cd6ff051e0afc564685f1de1cb1e341d52284
juanco2597 <juanparra2517@gmail.com>
2022-11-07 00:00:57 -0500

    añadiendo maquetacion y stilos

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,21 @@
+
+const Formulario = () => {
+    return (
+        <div className="md:w-1/2 lg:w-2/5">
+            <h2 className="font-black text-3xl text-center">
+                Seguimiento Pacientes
+            </h2>
+
+            <p className="text-lg mt-5 text-center">
+                Añade Pacientes y {''}
+                <span className="text-indigo-600 font-bold">Administrados</span>
+            </p>
+
+            <form action="">
+                
+            </form>
+        </div>
+    )
+}
+
+export default Formulario
